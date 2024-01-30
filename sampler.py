e5f40aae5ad5889966adc039ddc048b75e397d78
Tyas Nuur Khoolish <nuurkhoolish@gmail.com>
2024-01-30 10:53:15 +0700

    Add ingest and helper

--- sampler.py
+++ sampler.py
@@ -3,15 +3,11 @@ Use this code to generate sample data from SPOTIFY_REVIEWS.csv
 """
 
 import pandas as pd
-
+from helper import preprocess
 
 def sampler(path_str: str):
     df = pd.read_csv(path_str)
-    # Remove empty review text
-    df = df.dropna(subset=["review_text"])
-    # Remove {} in author_name and review_text
-    df["author_name"] = df["author_name"].apply(lambda x: x.replace("{","").replace("}", ""))
-    df["review_text"] = df["review_text"].apply(lambda x: x.replace("{","").replace("}", ""))
+    df = preprocess(df)
 
     df_sampled = df.groupby("review_rating", group_keys=False).apply(
         lambda x: x.sample(frac=0.0001, random_state=2024)
